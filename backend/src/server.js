ee51865250ce67eaed3095dbed345d928293f8c6
Leelasandeep <nls8656@gmail.com>
2025-07-06 23:37:57 +0530

    error1 after deployment fix 2

--- backend/src/server.js
+++ backend/src/server.js
@@ -27,10 +27,10 @@ app.use("/api/notes", notesRoutes);
 if (process.env.NODE_ENV === "production") {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../../frontend/dist", "index.html"));
   });
 }
 
